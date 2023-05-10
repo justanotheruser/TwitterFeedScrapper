d2878a0cfa885f97ef2aa94ca204f9d7f98152c5
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-05-10 13:24:39 +0300

    Refactoring + protection against StaleElementReferenceException

--- scrapping/scraper.py
+++ scrapping/scraper.py
@@ -2,6 +2,8 @@ import logging
 from datetime import datetime
 from typing import Optional
 
+from selenium.common.exceptions import StaleElementReferenceException
+
 from scrapping.driver_utils import Utilities
 from scrapping.element_finder import Finder
 from scrapping.login import login
@@ -32,8 +34,11 @@ class Scrapper:
         following_tab = Utilities.wait_until_element_appears(self.driver, FOLLOWING_XPATH, "'Following' tab")
         if not following_tab:
             return None
-        # TODO: check for modal window
-        following_tab.click()
+        try:
+            # TODO: check for modal window
+            following_tab.click()
+        except Exception as e:
+            logger.exception(f"Exception on clicking 'Following' tab: {e}")
         timeline = Utilities.wait_until_tweets_appear(self.driver)
         if not timeline:
             return None
@@ -43,62 +48,58 @@ class Scrapper:
 
     def __fetch_and_store_data(self):
         try:
-            already_fetched_posts = []
-            present_tweets = Finder.find_all_tweets(self.driver)
-            self.__check_tweets_presence(present_tweets)
-            already_fetched_posts.extend(present_tweets)
-
-            found_all_new_tweets = False
-            while not found_all_new_tweets:
-                for tweet in present_tweets:
-                    status, tweet_url = Finder.find_status(tweet)
-                    replies = Finder.find_replies(tweet)
-                    retweets = Finder.find_shares(tweet)
-                    status = status[-1]
-                    username = tweet_url.split("/")[3]
-                    is_retweet = Finder.is_retweet(tweet)
-                    posted_time = Finder.find_timestamp(tweet)
-                    content = Finder.find_content(tweet)
-                    likes = Finder.find_like(tweet)
-                    link = Finder.find_external_link(tweet)
-                    tweet_datetime = Finder.find_timestamp(tweet)
-                    # self.tweeted_after <= tweet_datetime < self.tweeted_before
-                    if not is_retweet and self.tweeted_before and tweet_datetime >= self.tweeted_before:
-                        continue
-                    if not is_retweet and tweet_datetime < self.tweeted_after:
-                        found_all_new_tweets = True
-                        break
-                    self.posts_data[status] = {
-                        "tweet_id": status,
-                        "username": username,
-                        "replies": replies,
-                        "retweets": retweets,
-                        "likes": likes,
-                        "is_retweet": is_retweet,
-                        "posted_time": posted_time,
-                        "content": content,
-                        "tweet_url": tweet_url,
-                        "link": link,
-                        "datetime": tweet_datetime
-                    }
-                Utilities.scroll_down(self.driver)
+            already_processed_tweets = []
+            while self.retry > 0:
                 Utilities.wait_until_completion(self.driver)
                 Utilities.wait_until_tweets_appear(self.driver)
-                present_tweets = Finder.find_all_tweets(self.driver)
-                present_tweets = [
-                    post for post in present_tweets if post not in already_fetched_posts]
-                self.__check_tweets_presence(present_tweets)
-                already_fetched_posts.extend(present_tweets)
-                if self.__check_retry():
+                all_tweets = Finder.find_all_tweets(self.driver)
+                if len(all_tweets) == 0:
+                    self.retry -= 1
+                    continue
+                new_tweets = [post for post in all_tweets if post not in already_processed_tweets]
+                try:
+                    found_all_new_tweets = self.parse_tweets(new_tweets)
+                except StaleElementReferenceException:
+                    self.retry -= 1
+                    continue
+                if found_all_new_tweets:
                     break
+                already_processed_tweets.extend(new_tweets)
+                Utilities.scroll_down(self.driver)
 
         except Exception as ex:
             logger.exception(
                 "Error at method fetch_and_store_data : {}".format(ex))
 
-    def __check_tweets_presence(self, tweet_list):
-        if len(tweet_list) <= 0:
-            self.retry -= 1
-
-    def __check_retry(self):
-        return self.retry <= 0
+    def parse_tweets(self, tweets):
+        for tweet in tweets:
+            status, tweet_url = Finder.find_status(tweet)
+            replies = Finder.find_replies(tweet)
+            retweets = Finder.find_shares(tweet)
+            status = status[-1]
+            username = tweet_url.split("/")[3]
+            is_retweet = Finder.is_retweet(tweet)
+            posted_time = Finder.find_timestamp(tweet)
+            content = Finder.find_content(tweet)
+            likes = Finder.find_like(tweet)
+            link = Finder.find_external_link(tweet)
+            tweet_datetime = Finder.find_timestamp(tweet)
+            # self.tweeted_after <= tweet_datetime < self.tweeted_before
+            if not is_retweet and self.tweeted_before and tweet_datetime >= self.tweeted_before:
+                continue
+            if not is_retweet and tweet_datetime < self.tweeted_after:
+                return True
+            self.posts_data[status] = {
+                "tweet_id": status,
+                "username": username,
+                "replies": replies,
+                "retweets": retweets,
+                "likes": likes,
+                "is_retweet": is_retweet,
+                "posted_time": posted_time,
+                "content": content,
+                "tweet_url": tweet_url,
+                "link": link,
+                "datetime": tweet_datetime
+            }
+        return False
